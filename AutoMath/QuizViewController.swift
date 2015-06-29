b3a616fe7207650a8f571929420aa573e6714a57
YUMA KOMORI <cubesugar78@gmail.com>
2015-06-29 21:38:53 +0900

    6/29 commit

--- AutoMath/QuizViewController.swift
+++ AutoMath/QuizViewController.swift
@@ -21,9 +21,78 @@ class QuizViewController: UIViewController {
         if message == "1" {
             println("hoge")
         }else if message == "2"{
-            println("hogehoge")
+            //UIImageに画像の名前を指定します
+//            let imgQ1 = UIImage(named: "3-1.png")
+            let imgQ2 = UIImage(named: "2-2.png")
+            let imgQ3 = UIImage(named: "2-3.png")
+            
+            //UIImageViewにUIIimageを追加
+//            let Q1 = UIImageView(image: imgQ1)
+            let Q2 = UIImageView(image: imgQ2)
+            let Q3 = UIImageView(image: imgQ3)
+            
+            //UIScrollViewを作成します
+            let scrView = UIScrollView()
+            
+            //UIScrollViewの1ページ分のサイズ + 表示位置
+            scrView.frame = CGRectMake(0, 50, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //全体のサイズ
+            scrView.contentSize = CGSizeMake(self.view.frame.width*3, self.view.frame.height*2/3)
+            
+            //UIImageViewのサイズと位置を決めます
+//            Q1.frame = CGRectMake(0, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q2.frame = CGRectMake(self.view.frame.width, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q3.frame = CGRectMake(self.view.frame.width*2, 10, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //viewに追加します
+            self.view.addSubview(scrView)
+            
+            //UIImageViewをScrollViewに追加します
+//            scrView.addSubview(Q1)
+            scrView.addSubview(Q2)
+            scrView.addSubview(Q3)
+            
+            // １ページ単位でスクロールさせる
+            scrView.pagingEnabled = true
         }else if message == "3"{
-            println("hogehogehoge")
+            //UIImageに画像の名前を指定します
+            let imgQ1 = UIImage(named: "3-1.png")
+            let imgQ2 = UIImage(named: "3-2.png")
+            let imgQ3 = UIImage(named: "3-3.png")
+            
+            //UIImageViewにUIIimageを追加
+            let Q1 = UIImageView(image: imgQ1)
+            let Q2 = UIImageView(image: imgQ2)
+            let Q3 = UIImageView(image: imgQ3)
+            
+            //UIScrollViewを作成します
+            let scrView = UIScrollView()
+            
+            //UIScrollViewの1ページ分のサイズ + 表示位置
+            scrView.frame = CGRectMake(0, 50, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //全体のサイズ
+            scrView.contentSize = CGSizeMake(self.view.frame.width*3, self.view.frame.height*2/3)
+            
+            //UIImageViewのサイズと位置を決めます
+            Q1.frame = CGRectMake(0, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q2.frame = CGRectMake(self.view.frame.width, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q3.frame = CGRectMake(self.view.frame.width*2, 10, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //viewに追加します
+            self.view.addSubview(scrView)
+            
+            //UIImageViewをScrollViewに追加します
+            scrView.addSubview(Q1)
+            scrView.addSubview(Q2)
+            scrView.addSubview(Q3)
+            
+            // １ページ単位でスクロールさせる
+            scrView.pagingEnabled = true
+            
+            
+
         }
         
         //グラデーション start
@@ -50,6 +119,8 @@ class QuizViewController: UIViewController {
         
         
         //グラデーション end
+        
+
 
 
         // Do any additional setup after loading the view.
