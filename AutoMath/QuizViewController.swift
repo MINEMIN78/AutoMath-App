8adbd67d305c05eee00118195a0155396b2edb4b
YUMA KOMORI <cubesugar78@gmail.com>
2015-06-30 00:20:41 +0900

    quiz commit

--- AutoMath/QuizViewController.swift
+++ AutoMath/QuizViewController.swift
@@ -14,12 +14,46 @@ class QuizViewController: UIViewController {
         super.viewDidLoad()
         
         
-        
+        //値受け渡し
         var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate //AppDelegateのインスタンスの取得
         var message = appDelegate.message
         
+        //問題配置
         if message == "1" {
-            println("hoge")
+            //UIImageに画像の名前を指定します
+            let imgQ1 = UIImage(named: "1-1.png")
+            let imgQ2 = UIImage(named: "1-2.png")
+            let imgQ3 = UIImage(named: "1-3.png")
+            
+            //UIImageViewにUIIimageを追加
+            let Q1 = UIImageView(image: imgQ1)
+            let Q2 = UIImageView(image: imgQ2)
+            let Q3 = UIImageView(image: imgQ3)
+            
+            //UIScrollViewを作成します
+            let scrView = UIScrollView()
+            
+            //UIScrollViewの1ページ分のサイズ + 表示位置
+            scrView.frame = CGRectMake(0, 50, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //全体のサイズ
+            scrView.contentSize = CGSizeMake(self.view.frame.width*3, self.view.frame.height*2/3)
+            
+            //UIImageViewのサイズと位置を決めます
+            Q1.frame = CGRectMake(0, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q2.frame = CGRectMake(self.view.frame.width, 10, self.view.frame.width, self.view.frame.height*2/3)
+            Q3.frame = CGRectMake(self.view.frame.width*2, 10, self.view.frame.width, self.view.frame.height*2/3)
+            
+            //viewに追加します
+            self.view.addSubview(scrView)
+            
+            //UIImageViewをScrollViewに追加します
+            scrView.addSubview(Q1)
+            scrView.addSubview(Q2)
+            scrView.addSubview(Q3)
+            
+            // １ページ単位でスクロールさせる
+            scrView.pagingEnabled = true
         }else if message == "2"{
             //UIImageに画像の名前を指定します
 //            let imgQ1 = UIImage(named: "3-1.png")
